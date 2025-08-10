313ba932fe69cc8072e08244d09c7c9a6e14ebc4
ninoslat1 <benedict.ellnina@gmail.com>
2025-08-10 16:00:08 +0700

    refactor: migrate to bun eco

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
+import bun from "@nurodev/astro-bun";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  }),
+  // adapter: node({
+  //   mode: 'standalone'
+  // }),
+  adapter: bun(),
+  output: "server",
   vite: {
       plugins: [tailwindcss()]
   },
